f55d1afcf75a91ae03bce9b70e83b4f2b5f7d16b
MFJcode <mfjcode@gmail.com>
2022-01-01 12:36:45 +0100

    criaErro

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,12 +20,22 @@ class ValidaFormulario {
         let valid = true;
 
         for(let campo of this.formulario.querySelectorAll('.validar')) {
+            const label = campo.previousElementSibling.innerText;
             if(!campo.value) {
-                this.criaErro(campo, 'Campo tal não pode estar em branco')
+                this.criaErro(campo, `Campo "${label}" não pode estar em branco`)
+                valid = false;
 
             }
         }
     }
+
+    criaErro(campo, msg) {
+            const div = document.createElement('div');
+            div.innerHTML = msg;
+            div.classList.add('error-text');
+            campo.insertAdjacentElement('afterend', div);
+
+    }    
 }
 
 
